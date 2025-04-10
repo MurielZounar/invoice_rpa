6ee4bf3078ebbb62c3b6eef7b168691e28a7c5f5
Muriel Zounar <muriel.zounar@gmail.com>
2025-04-10 12:48:02 -0300

    :sparkles: Add logging
    Added success and error logs. This logs are saved in 'orders.log' file

--- main.py
+++ main.py
@@ -17,8 +17,11 @@ invalid_orders = []
 def import_orders(df):
     for index, row in df.iterrows():
         try:
+            logging.info('-----------------------------------')
+            logging.info(f'Importando pedido {index + 2}...')
             new_invoice = Order(index=index + 2, name=row['Nome'], email=row['E-mail'], product=row['Produto'], quantity=row['Quantidade'], unit_value=row['Valor Unitário'])
             valid_orders.append(new_invoice)
+            logging.info('Pedido importado com sucesso')
         except ValueError as e:
             new_invoice = dict()
             new_invoice['index'] = index + 2
@@ -27,35 +30,34 @@ def import_orders(df):
             new_invoice['product'] = row['Produto']
             new_invoice['quantity'] = row['Quantidade']
             new_invoice['unitValue'] = row['Valor Unitário']
+            logging.info('Houve um erro ao importar o pedido.')
+            logging.info(f'Dados do pedido: {new_invoice}')
             invalid_orders.append(new_invoice)
 
-    os.system('cls')
-    msg = f'''************************************************************************************************************************
-- Total de pedidos importados: {len(valid_orders)}
-'''
-    print(msg)
+    logging.info('==============================================')
+    logging.info(f'- Total de pedidos importados: {len(valid_orders)}')
     
     if invalid_orders:
-        error_message = '''************************************************************************************************************************
-Os pedidos das linhas apresentadas abaixo contém inconsistências, por gentileza, verifique-os:\n'''
-        for invoice in invalid_orders:
-            error_message += f' - Linha {invoice['index']}\n'
-        
-        print(f'- Total de pedidos com erro: {len(invalid_orders)}\n')
-        print(error_message)
+        logging.info(f'- Total de pedidos com erro: {len(invalid_orders)}')
+    
+    logging.info('==============================================')
 
 def main():
     url = URL + SEND_ORDER_ROUTE
-    
+
+    logging.info('*********************************************************')
+    logging.info('Iniciando processo de importacao de tabela de pedidos')
+
     try:
         df = pd.read_excel('./files/orders.xlsx')
         import_orders(df)
     except Exception as e:
-        os.system('cls')
-        print(f'Houve um erro ao importar a planilha de pedidos.\nDescrição do erro:\n{e}')
+        logging.info(f'Houve um erro ao importar a planilha de pedidos.\nDescricao do erro:{e}\n')
 
     for current_order in valid_orders:
-        print(current_order.index)
+        logging.info('*********************************************************')
+        logging.info('Iniciando envio de pedidos')
+        
         order = {
             'index': current_order.index,
             'name': current_order.name,
@@ -65,14 +67,21 @@ def main():
             'unit_value': current_order.unit_value
         }
 
+        logging.info('---------------------------------------')
+        logging.info(f'Enviando pedido {order['index']}\n')
+        logging.info(f'Dados do pedido: {order}\n')
+
         response = requests.post(url, json=order)
 
         if response.status_code == 200:
-            print(f'Pedido {current_order.index} enviado com sucesso!\n\n')
+            logging.info(f'Pedido enviado com sucesso!\n')
         else:
-            print(f'Houve uma falha ao enviar o pedido {current_order.index}\nCódigo do erro: {response.status_code}\n')
+            logging.info(f'Houve uma falha ao enviar o pedido {current_order.index}\nCódigo do erro: {response.status_code}\n')
         
         sleep(2)
+    
+    logging.info('Fim do processo de importacao.')
+    logging.info('*********************************************************')
     
 if __name__ == '__main__':
     main()
